13a4ab372cbc9870a86862148ab8f4228bc583c8
Dinçer <Diner@DESKTOP-AIGUMKH>
2021-10-23 11:06:49 +0300

    Add project files.

---
+++ Lists/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class MyList<T>
+    {
+        T[] array=new T[0];
+        T[] temparray;
+
+        public int Count
+        {
+            get { return array.Length; }
+        }
+
+        public void Add(T item)
+        {
+            temparray = array;
+            array = new T[temparray.Length+1];
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                array[i] = temparray[i];
+            }
+            array[temparray.Length] = item;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("Ankara");
+            list.Add("Ankara");
+            list.Add("Ankara");
+            list.Add("Ankara");
+            list.Add("Ankara");
+
+            Console.WriteLine(list.Count);
+
+            MyList<string> mylist = new MyList<string>();
+            mylist.Add("Ankara");
+            mylist.Add("Ankara");
+            mylist.Add("Ankara");
+            mylist.Add("Ankara");
+            mylist.Add("Ankara");
+            mylist.Add("Ankara");
+            mylist.Add("Ankara");
+            mylist.Add("Ankara");
+            mylist.Add("Ankara");
+            mylist.Add("Ankara");
+
+            Console.WriteLine(mylist.Count);
+            
+
+            //int[] array = new int[1];
+            //Console.WriteLine(array.Length);
+            Console.ReadKey();
+        }
+    }
+}
